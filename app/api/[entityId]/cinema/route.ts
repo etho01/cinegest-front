68345bc72920263cf7acbb528dbc022921b24970
nicolas <nicolas@pcfixenico.home>
2025-11-08 12:28:07 +0100

    fix warning api

--- app/api/[entityId]/cinema/route.ts
+++ app/api/[entityId]/cinema/route.ts
@@ -1,14 +1,18 @@
 import { getCinemas } from "@/src/application/useCases/Cinema/getCinemas";
 import { CinemaRepositoryImpl } from "@/src/infrastructure/repositories/CinemaRepositoryImpl";
 
+interface GetCinemasApiProps {
+    params: Promise<{ entityId: number }>;
+}
 
-export async function GET(req : Request, { params } : { params: { entityId: string }}) {
+export async function GET(req : Request, { params } : GetCinemasApiProps) {
     const { searchParams } = new URL(req.url);
+    const paramsObj = await params;
     
     const page = Number(searchParams.get('page')) ?? 1;
     const search = searchParams.get('search') ?? '';
 
-    const cinemas = await getCinemas(CinemaRepositoryImpl, {entityId: Number(params.entityId), page, search})
+    const cinemas = await getCinemas(CinemaRepositoryImpl, {entityId: paramsObj.entityId, page, search})
 
     return Response.json(cinemas);
 }
