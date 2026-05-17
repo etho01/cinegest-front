f7a27b06230c2676a569d70151642a8495c4f1ed
Nicolas <barbeynicolas.basly@gmail.com>
2026-05-17 16:03:44 +0200

    test for identity bug coockie delete prod

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(req: NextRequest) {
     // Pages publiques (pas besoin d'être connecté)
     const publicPaths = ['/login', '/forgot-password', '/reset-password'];
     const isPublicPath = publicPaths.some(path => pathname.startsWith(path));
-    console.log(`Middleware - Path: ${pathname}, isLog: ${isLog}, isPublicPath: ${isPublicPath}`);
+    console.log(`Middleware - Path: ${pathname}, isLog: ${isLog}, isPublicPath: ${isPublicPath}`, cookieStore.get('login-token')?.value, cookieStore.has('login-token'));
 
     if (isPublicPath && isLog)
     {
